1cb3a4003735bb0520b8bce08fcc18985e36c098
Sandra Gao <sgao60@stuy.edu>
2026-01-06 23:13:42 -0500

    update library : event + date structs ; display_calendar() ; create_event()

--- client_behavior.h
+++ client_behavior.h
@@ -1,5 +1,19 @@
 #ifndef CLIENT_BEHAVIOR_H
 #define CLIENT_BEHAVIOR_H
 
+struct event {
+    char name[128];
+    char description[256];
+    int permissions; // -1 : private ; 0 : public
+    int all_day; // nonzero : all day ; 0 : time block
+};
+
+struct date {
+    int month, day, year;
+    struct event* events;
+};
+
+void display_calendar(int month);
+struct event* create_event(char* name, char* description, int permissions, int all_day);
 
 #endif
