c81a470fe5c7dc19aedc02d784bf354a87398db0
eileenlee0214 <iamliah1106@gmail.com>
2026-01-08 15:06:57 -0500

    see if this merge works

--- server_behavior.c
+++ server_behavior.c
@@ -11,7 +11,7 @@ void sighandler(int signo) {
 
         shmdt(shared_calendar);
         shmctl(shmid, IPC_RMID, NULL);
-        
+
         exit(0);
         exit(0);
     }
@@ -20,7 +20,7 @@ void sighandler(int signo) {
 struct DateNode* find_or_create_date(int month, int day, int year, struct SharedCalendar *shared_cal) {
     struct DateNode *curr = shared_cal->calendar_head;
     struct DateNode *prev = NULL;
-    
+
     while (curr) {
         if (curr->year == year && curr->month == month && curr->day == day) {
             return curr;
@@ -28,35 +28,35 @@ struct DateNode* find_or_create_date(int month, int day, int year, struct Shared
         prev = curr;
         curr = curr->next;
     }
-    
+
     struct DateNode *new_date = malloc(sizeof(struct DateNode));
     new_date->month = month;
     new_date->day = day;
     new_date->year = year;
     new_date->events = NULL;
     new_date->next = NULL;
-    
+
     if (shared_cal->calendar_head == NULL) {
         shared_cal->calendar_head = new_date;
     } else {
         prev->next = new_date;
     }
-    
+
     return new_date;
 }
 
 void handle_client_message(int client_socket, int client_id, struct SharedCalendar *shared_cal) {
     char buffer[BUFFER_SIZE];
     memset(buffer, 0, BUFFER_SIZE);
-    
+
     int bytes_read = read(client_socket, buffer, BUFFER_SIZE - 1);
-    
+
     if (bytes_read <= 0) {
         printf("Client %d disconnected\n", client_id);
         close(client_socket);
         return;
     }
-    
+
     // process_command(buffer, client_socket, client_id, shared_cal);
 }
 
@@ -117,10 +117,11 @@ void save_calendar(struct SharedCalendar *shared_cal) {
                     event->name, event->description, event->owner_id,
                     event->permissions, event->all_day);
 
-            fprintf(fp, "%d|%d|%d|%d\n",
+            /* fprintf(fp, "%d|%d|%d|%d\n",
                     event->start_hour, event->start_minute,
                     event->end_hour, event->end_minute);
             event = event->next;
+            */
         }
         date = date->next;
     }
@@ -139,4 +140,3 @@ void load_calendar() {
     fclose(fp);
 }
     */
-
