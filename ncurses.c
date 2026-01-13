3815d8a4512324ab40a58f01dbac2a874ceb4f60
eileenlee0214 <iamliah1106@gmail.com>
2026-01-13 15:35:30 -0500

    cw 13 jan

---
+++ ncurses.c
@@ -0,0 +1,18 @@
+#include "networking.h"
+#include "client_behavior.h"
+#include "text.h"
+#include <stdbool.h>
+#include <time.h>
+#include <sys/time.h>
+
+#define FRAME_RATE 60
+
+int main() {
+  initscr();
+
+  bool running = true;
+  while (running) {
+  clear
+  }
+
+}
