bc35564c843a43e015a8ff8a49a89e7837063f20
Sandra Gao <sgao60@stuy.edu>
2026-01-13 01:57:29 -0500

    adding more to terminal manipulation

--- text.c
+++ text.c
@@ -1,9 +1,47 @@
 #include <stdio.h>
+#include "text.h"
 
 void clear() {
-    printf("\x1b[2J");
+  printf("\x1b[2J");
 }
 
 void go(int row, int col) {
-    printf("\x1b[%d;%df", row, col);
+  printf("\x1b[%d;%df", row, col);
+}
+
+void reset() {
+  printf("\x1b[0m");
+}
+
+void hideCursor() {
+  printf("\x1b[?25l");
+}
+
+void showCursor() {
+  printf("\x1b[?25h");
+}
+
+void clear_block(int row, int col, int width, int height) {
+  char eraser[width + 1]; eraser[width] = 0;
+  for (int i = 0; i < width; i++) {
+    eraser[i] = ' ';
+  }
+
+  for(int i = 0; i < height; i++) {
+    go(row + i,col);
+    printf("%s", eraser);
+  }
+}
+
+  /*Overloaded Colorize methods.
+  c1,c2 and c3 are any color modifiers such as bold/color/background color etc.
+  */
+void color(char* text, int num_mods, int* mods) {
+  printf("\x1b[");
+
+  for (int i = 0; i < num_mods; i++) {
+    printf("%d", mods[i]);
+    if (i != num_mods - 1) printf(";");
+  }
+  printf("m%s\x1b[0m", text);
 }
--- text.h
+++ text.h
@@ -1,7 +1,34 @@
 #ifndef TEXT_H
 #define TEXT_H
 
+#define terminal_width 169
+#define terminal_height 45
+
+/*Base colors*/
+#define RED 31;
+#define BLACK 30;
+#define GREEN 32;
+#define YELLOW 33;
+#define BLUE 34;
+#define MAGENTA 35;
+#define CYAN 36;
+#define WHITE 37;
+
+/*Text modifiers to be ADDED to a color*/
+#define BACKGROUND 10;
+#define BRIGHT 60;
+
+/*Text modifiers that are separate from color*/
+#define BOLD 1;
+#define UNDERLINE 4;
+#define INVERTED 7;
+
 void clear();
-void go (int row, int col);
+void go(int row, int col);
+void reset();
+void hideCursor();
+void showCursor();
+void clear_block(int row, int col, int width, int height);
+void color(char* text, int num_mods, int* mods);
 
 #endif
