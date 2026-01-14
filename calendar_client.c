9bb8588f9a6c62841fbd7940c739dc1e20f0d2ce
eileenlee0214 <iamliah1106@gmail.com>
2026-01-14 14:49:54 -0500

    clearing display fixed

--- calendar_client.c
+++ calendar_client.c
@@ -13,59 +13,48 @@ int main(int argc, char** argv) {
   if (argc > 1) ip = argv[1];
 
   int server_socket = client_tcp_handshake(ip);
-  printf("client connected.\n");
 
   char buffer[BUFFER_SIZE];
   memset(buffer, 0, BUFFER_SIZE);
   read(server_socket, buffer, BUFFER_SIZE - 1);
-  printf("%s", buffer);
-
-  sleep(1);
-  init_ui();
+  
+  time_t raw_now = time(NULL);
+  struct tm* now = localtime(&raw_now);
+  display_calendar(now);
+  
+  go(1, 1);
+  printf("Connected: %s", buffer);
 
   while (1) {
-        clear();
-        time_t raw_now = time(NULL);
-        struct tm* now = localtime(&raw_now);
+        print_prompt();
+        fflush(stdout);
 
-        display_calendar(now);
-
-        move(terminal_height - 2, 0);
-        printw("Commands: CREATE, VIEW, MONTH, DELETE (or 'quit' to exit)");
-        move(terminal_height - 1, 0);
-        printw("enter command: ");
-        refresh();
-
-        char command[BUFFER_SIZE];
-        get_command_with_history(command, BUFFER_SIZE);
-
-        if (strcmp(command, "quit") == 0 || strcmp(command, "exit") == 0) {
+        memset(buffer, 0, BUFFER_SIZE);
+        if (fgets(buffer, BUFFER_SIZE, stdin) == NULL) {
+            break;
+        }
+        
+        if (strncmp(buffer, "quit", 4) == 0) {
             break;
         }
 
-        add_to_history(command);
-
-        strcat(command, "\n");
-        write(server_socket, command, strlen(command));
+        write(server_socket, buffer, strlen(buffer));
 
         memset(buffer, 0, BUFFER_SIZE);
         int bytes_read = read(server_socket, buffer, BUFFER_SIZE - 1);
         if (bytes_read <= 0) {
-            mvprintw(terminal_height - 3, 0, "Server disconnected");
-            refresh();
-            getch();
+            printf("Server disconnected\n");
             break;
         }
-
-        clear();
-        mvprintw(0, 0, "Server Response:");
-        mvprintw(1, 0, "%s", buffer);
-        mvprintw(terminal_height - 1, 0, "Press any key to continue...");
-        refresh();
-        getch();
+        go(terminal_height - 3, 0);
+        printf("%s", buffer);
+        
+        time_t raw_now = time(NULL);
+        struct tm* now = localtime(&raw_now);
+        display_calendar(now);
     }
 
-    cleanup_ui();
+
     close(server_socket);
     return 0;
 }
--- text.h
+++ text.h
@@ -4,7 +4,6 @@
 #define terminal_width 169
 #define terminal_height 45
 
-/*Base colors*/
 #define RED 31;
 #define BLACK 30;
 #define GREEN 32;
@@ -14,11 +13,9 @@
 #define CYAN 36;
 #define WHITE 37;
 
-/*Text modifiers to be ADDED to a color*/
 #define BACKGROUND 10;
 #define BRIGHT 60;
 
-/*Text modifiers that are separate from color*/
 #define BOLD 1;
 #define UNDERLINE 4;
 #define INVERTED 7;
