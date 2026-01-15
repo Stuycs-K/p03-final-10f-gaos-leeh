83f4fa8331f980e5ff675b056b4f87bda050c336
Sandra Gao <sgao60@stuy.edu>
2026-01-15 15:06:12 -0500

    merge conflicts

--- ui.h
+++ ui.h
@@ -8,5 +8,5 @@ void cleanup_ui();
 int get_days_in_month(int month, int year);
 void add_to_history(const char *cmd);
 void get_command_with_history(char *buffer, int max_len);
-
+void display_calendar_ncurses(struct tm* display_time);
 #endif
