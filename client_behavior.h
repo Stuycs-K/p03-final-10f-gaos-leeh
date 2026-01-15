ed59d267d70d5499185d625bc0289bf7b0a63fc5
Sandra Gao <sgao60@stuy.edu>
2026-01-15 06:43:15 -0500

    ui library

--- client_behavior.h
+++ client_behavior.h
@@ -2,18 +2,12 @@
 #define CLIENT_BEHAVIOR_H
 
 #include <time.h>
-#include <ncurses.h>
 #include "calendar.h"
+#include "ui.h"
 
-void display_calendar(struct tm*, int shift);
+void display_calendar(struct tm*);
 void print_prompt();
 struct tm* get_now();
 struct EventNode* create_event(int owner_id, char* name, char* description, int permissions, int* times);
-
-void init_ui();
-void cleanup_ui();
-int get_days_in_month(int month, int year);
-void add_to_history(const char *cmd);
-void get_command_with_history(char *buffer, int max_len);
 
 #endif
